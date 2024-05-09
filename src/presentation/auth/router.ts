add0e6f195bdaf024696eccab4b44b3f0da42214
Sebastian <agudelocortess@gmail.com>
2024-05-09 13:26:32 -0500

    Implementación logica de consumo del AuthRepository

--- src/presentation/auth/router.ts
+++ src/presentation/auth/router.ts
@@ -1,17 +1,22 @@
-
 import { Router } from 'express';
 import { AuthController } from './controller'; // 1
+import { AuthDataSourceImpl, AuthRepositoryImpl } from '../../infraestructure'; // 5
 
 export class AuthRoutes {
+
 
     static get routes(): Router {
   
       const router = Router();
-      const controller = new AuthController(); // 2
+
+      const datasource = new AuthDataSourceImpl(); //6
+      const AuthRepository = new AuthRepositoryImpl(datasource); //7
+      const controller = new AuthController(AuthRepository); // 2 // 8 actulizacion
   
         // Definir todas mis rutas especificas
         router.post('/login', controller.loginUser) // 3
         router.post('/register', controller.registerUser) // 4
+  
   
         return router;
     }
