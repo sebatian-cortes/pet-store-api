4d3fa3a8977ed2fb5212185c55a77d1c70526bd6
Sebastian <agudelocortess@gmail.com>
2024-06-13 13:12:57 -0500

    Implementacion Manejo de errores en el controlador

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'; // 5
-import { RegisterUserDto, AuthRepository } from './../../domain'; // 1
+import { AuthRepository, CustomError,  RegisterUserDto } from '../../domain';
 
 
 
@@ -16,11 +16,30 @@ export class AuthController { // 1
 
         this.authRepository.register(registerUserDto!) //10
         .then( user => res.json(user) ) // 11
-        .catch( error => res.status(500).json(error) ) // 12
+        .catch((error) => this.handleError(error, res)); // 12
     } // 3
 
     loginUser =  (req: Request, res: Response) => { // 7
         res.json('loginUser controller') // 8
     } // 6
+
+    /* La función `private handleError` en la clase `AuthController` es un método utilizado para manejar
+errores que pueden ocurrir durante la ejecución de ciertas operaciones, como el registro de un
+usuario. Aquí hay un desglose de lo que hace: */
+private handleError = ( error: unknown, res: Response ) => {
+    
+    /* El fragmento de código `if (instancia de error de CustomError)` comprueba si el objeto
+    `error` es una instancia de la clase `CustomError`. Si el objeto "error" es una instancia de
+    "CustomError", significa que el error se generó intencionalmente como un error personalizado
+    dentro de la aplicación. */
+     if ( error instanceof CustomError ) {
+       return res.status(error.statusCode).json({ error: error.message });
+     }
+
+     /* La línea `return res.status(500).json({ error: 'Error interno del servidor' });` en el
+     método `handleError` de la clase `AuthController` es responsable de enviar una respuesta
+     JSON con un mensaje de error que indica una Error Interno del Servidor. */
+     return res.status(500).json({ error: 'Internal Server Error' });
+   }
 
 }
